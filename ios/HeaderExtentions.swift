449f4036893a17f226546af50f1271239621f704
HungTran <tdh4vn@gmail.com>
2018-01-02 09:29:08 +0700

    Final

---
+++ ios/HeaderExtentions.swift
@@ -0,0 +1,9 @@
+//
+//  HeaderExtentions.swift
+//  NativeStarterKit
+//
+//  Created by HungTran on 12/8/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+import Foundation
---
+++ ios/NativeStarterKit-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  NativeStarterKit-Bridging-Header.h
+//  NativeStarterKit
+//
+//  Created by HungTran on 12/8/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#ifndef NativeStarterKit_Bridging_Header_h
+#define NativeStarterKit_Bridging_Header_h
+
+
+#endif /* NativeStarterKit_Bridging_Header_h */
